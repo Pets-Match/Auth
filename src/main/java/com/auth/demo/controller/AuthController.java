9b9dece3e1bc5f46b8dc585057df9d676de80b95
Gustavo Diogo <gustavost1y@gmail.com>
2022-11-28 22:00:01 -0300

    feat: middleware

--- src/main/java/com/auth/demo/controller/AuthController.java
+++ src/main/java/com/auth/demo/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.auth.demo.controller;
 
-
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
@@ -14,16 +18,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.auth.demo.dto.AuthDTO;
-import com.auth.demo.dto.RoleDTO;
 import com.auth.demo.dto.RoleToUserDTO;
 import com.auth.demo.dto.UserDTO;
 import com.auth.demo.model.User;
 import com.auth.demo.service.UserService;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthController {
-
+    static String secret = "yjI5BMKPBV55bhp4hqIiVUSxWFiYElL2HU213Y7128JS1289IKO";
     private final UserService userService;
 
     public AuthController(UserService userService){
@@ -32,28 +40,42 @@ public class AuthController {
 
     @GetMapping()
     public ResponseEntity<String> auth(@RequestBody AuthDTO auth) {
-        User user = userService.findUserByEmail(auth.getEmail());
+        User usuario = userService.findUserByEmail(auth.getEmail());
 
-        if(auth.getEmail() == null || auth.getPassword() == null){
-            return new ResponseEntity<String>("Invalid Body Request, either email or password is null ", HttpStatus.BAD_REQUEST);
+        if (usuario == null) {
+            return new ResponseEntity<String>("Credentials not found ", HttpStatus.BAD_REQUEST);
         }
 
-        if (user != null) {
+        try {
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            Boolean verify = passwordEncoder.matches(auth.getPassword(), usuario.getPassword());
 
-            Boolean verify = passwordEncoder.matches(auth.getPassword(), user.getPassword());
+            if (verify) {
+                String jwtToken = Jwts.builder()
+                        .setSubject(usuario.getEmail())
+                        .setIssuer("localhost:8080")
+                        .setIssuedAt(new Date())
+                        .setExpiration(
+                                Date.from(
+                                        LocalDateTime.now().plusMinutes(15L)
+                                                .atZone(ZoneId.systemDefault())
+                                                .toInstant()))
+                        .signWith(SignatureAlgorithm.HS256, secret)
+                        .compact();
+                JSONObject json = new JSONObject();
+                json.put("token", jwtToken);
+                json.put("id", usuario.getId());
 
-            if(verify){
-                JSONObject json = new JSONObject(user);
                 return new ResponseEntity<String>(json.toString(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<String>("Credentials not found", HttpStatus.BAD_REQUEST);
+
             }
-
-            return new ResponseEntity<String>("Not found", HttpStatus.BAD_REQUEST);
-        } else {
-            return new ResponseEntity<String>("Not found", HttpStatus.BAD_REQUEST);
+        } catch (Exception ex) {
+            return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
         }
-
     }
+
 
     @PostMapping
     public ResponseEntity<String> create(@RequestBody UserDTO userDTO) {
@@ -79,5 +101,34 @@ public class AuthController {
     public ResponseEntity<List<User>> getUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+
+
+    @GetMapping("/middleware")
+    public ResponseEntity<String> middleware(HttpServletRequest request) {
+        try {
+
+            String authorizationHeader = request.getHeader("Authorization");
+            String token = authorizationHeader.substring("Bearer".length()).trim();
+
+            Jws<Claims> jwt = Jwts.parserBuilder()
+                    .setSigningKey(secret)
+                    .build()
+                    .parseClaimsJws(token);
+            JSONObject json = new JSONObject(jwt);
+            JSONObject jsonCringe = (JSONObject) json.get("body");
+
+            User user = userService.findUserByEmail(jsonCringe.getString("sub"));
+
+            JSONObject response = new JSONObject(user);
+
+            return new ResponseEntity<String>(response.toString(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
 
 }
